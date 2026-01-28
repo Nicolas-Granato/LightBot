16096ad39d00382b22ac7fb315f47a0fd5e6ed7e
Nicolas Barbosa <ngranato11@gmail.com>
2026-01-28 11:22:14 -0300

    refatorando...

--- js/main.js
+++ js/main.js
@@ -1,36 +1,4 @@
-const robo = {
-    image: document.querySelector(personagem > img),
-    aceso: false,
-    direcao: {
-        direta: 0 || 360,
-        cima: 90,
-        esquerda: 180,
-        baixo: 270
-    },
-    estados: ["apagado/parado","acelerando","vmaxima","desacelerando","acendendo"],
-
-    girarDireita() {
-        this.angle += 45;
-        this.image.style.transform = `rotate(${this.angle}deg)`
-        setTimeout(() => {},300);
-        this.angle += 45;
-        this.angle.style.transform = `rotate(${this.angle}deg)`
-    },
-
-    girarEsquerda() {
-        this.angle -= 45;
-        this.image.style.transform = `rotate(${this.angle}deg)`;
-        setTimeout(() => {},300);
-        this.angle -= 45;
-        this.angle.style.transform = `rotate(${this.angle}deg)`;
-    },
-
-    acender() {
-        parado = "images\\roboSprite0.png";
-        acender0 = "images\\roboSpriteAcendendo0.png";
-        acender1 = "images\\roboSpriteAcendendo1.png";
-        this.image.style.transform = `rotate(${this.angle}deg)`;
-
-        this.image.src = parado 
-    }
-}
+const roboImg = documento.querySelector("personagem img");
+const botoesControle = documento.querySelector(".controles")
+const listaVisual = documento.querySelector("listaDeComandos");
+const botaoEnviar = document.querySelector(".enviar");
